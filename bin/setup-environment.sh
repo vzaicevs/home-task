06f7ab8c36a4c51f57fdd40873bbf9430086fc99
Vyacheslav Zaytsev <vyacheslav.zaytsev@ecentria.com>
2024-01-29 00:08:24 +0200

    Fix Ubuntu / CentOS detection

--- bin/setup-environment.sh
+++ bin/setup-environment.sh
@@ -105,13 +105,13 @@ print_info "Checking terraform.."
 if ! command -v terraform >/dev/null; then
   print_warning "Terraform is not found, installing"
 
-  if ! lsb_release -si | grep -q -e Ubuntus -e Debian ; then
+  if command -v apt ; then
     sudo apt-get update && sudo apt-get install -y gnupg software-properties-common curl
     curl -fsSL https://apt.releases.hashicorp.com/gpg | sudo apt-key add -
     sudo apt-add-repository "deb [arch=amd64] https://apt.releases.hashicorp.com $(lsb_release -cs) main"
     sudo apt-get update && sudo apt-get install terraform
     print_success "Terraform installed"
-  elif ! command -v yum ; then
+  elif command -v yum ; then
     sudo yum install -y yum-utils
     sudo yum-config-manager --add-repo https://rpm.releases.hashicorp.com/RHEL/hashicorp.repo
     sudo yum -y install terraform
